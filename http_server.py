688be729ea860e2169d91c04d4b66cbfbb4409c2
zulufoxtrot <youri@tbc-france.com>
2023-01-20 20:39:35 +0100

    HTTP server: add charge, climate and doors controls

--- http_server.py
+++ http_server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 
 from VehicleClient import VehicleClient
 
@@ -9,12 +9,64 @@ app = Flask(__name__)
 
 @app.route("/force_refresh")
 def force_refresh():
-    vehicle_client = VehicleClient()
-    vehicle_client.vm.check_and_refresh_token()
-    vehicle_client.vehicle = vehicle_client.vm.get_vehicle(os.environ["KIA_VEHICLE_UUID"])
     vehicle_client.vm.force_refresh_vehicle_state(vehicle_client.vehicle.id)
     return "OK"
 
 
+@app.route("/status")
+def get_cached_status():
+    vehicle_client.vm.check_and_refresh_token()
+    return str(vehicle_client.vehicle)
+
+
+@app.route("/climate")
+def toggle_climate():
+    vehicle_client.vm.check_and_refresh_token()
+
+    action = request.args.get('action')
+    if action == "start":
+        vehicle_client.vm.start_climate(vehicle_client.vehicle.id)
+        return "Climate control ON"
+    elif action == "stop":
+        vehicle_client.vm.stop_climate(vehicle_client.vehicle.id)
+        return "Climate control OFF"
+    else:
+        return f"unrecognised command: {action}. send start or stop."
+
+
+@app.route("/charge")
+def toggle_charge():
+    vehicle_client.vm.check_and_refresh_token()
+
+    action = request.args.get('action')
+    if action == "start":
+        vehicle_client.vm.start_charge(vehicle_client.vehicle.id)
+        return "Charge ON"
+    elif action == "stop":
+        vehicle_client.vm.stop_charge(vehicle_client.vehicle.id)
+        return "Charge OFF"
+    else:
+        return f"unrecognised command: {action}. send start or stop."
+
+
+@app.route("/doors")
+def toggle_charge():
+    vehicle_client.vm.check_and_refresh_token()
+
+    action = request.args.get('action')
+    if action == "lock":
+        vehicle_client.vm.lock(vehicle_client.vehicle.id)
+        return "Doors LOCKED"
+    elif action == "unlock":
+        vehicle_client.vm.unlock(vehicle_client.vehicle.id)
+        return "Doors UNLOCKED"
+    else:
+        return f"unrecognised command: {action}. send lock or unlock."
+
+
 if __name__ == "__main__":
+    vehicle_client = VehicleClient()
+    vehicle_client.vm.check_and_refresh_token()
+    vehicle_client.vehicle = vehicle_client.vm.get_vehicle(os.environ["KIA_VEHICLE_UUID"])
+
     app.run(port=8000, host='0.0.0.0', debug=True)
