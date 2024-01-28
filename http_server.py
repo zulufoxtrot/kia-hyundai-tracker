bea4dea1be7f376ff43de228b67ca9c64b814a2b
zulufoxtrot <youri.lcd@gmail.com>
2024-01-28 21:36:37 +0100

    a

--- http_server.py
+++ http_server.py
@@ -180,7 +180,7 @@ def toggle_climate_async():
             args=(action, options),
             daemon=True)
 
-    return jsonify({"component": "climate", "action": action, "status": status})
+    return jsonify({"component": "climate", "action": action, "status": status.__str__()})
 
 
 def perform_climate_async_action(action: str, options: ClimateRequestOptions):
